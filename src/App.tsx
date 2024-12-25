1216d4b962e05e8d40975bffa714e12a3596d19b
WizzyCodes <wisdomiberi832@gmail.com>
2024-12-25 11:16:43 +0100

    processing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import Mije from "./components/Mije";
+import Wisdom from "./components/Wisdom";
 
 const App = () => {
   return (
     <div>
-      <h1>
-        <Mije />
-      </h1>
+      <Mije />
+      <Wisdom />
     </div>
   );
 };
--- src/components/Wisdom.tsx
+++ src/components/Wisdom.tsx
@@ -1,7 +1,30 @@
-import React from "react";
+import { useRef } from "react";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
-const wisdom = () => {
-  return <div>wisdom</div>;
+const Wisdom = () => {
+  const container: any = useRef();
+
+  useGSAP(
+    () => {
+      gsap.from(".box", { rotation: 180, duration: 2 });
+    },
+    { scope: container }
+  );
+
+  return (
+    <div
+      ref={container}
+      className=" w-full min-w-[696px] aspect-[1448/708] relative flex"
+    >
+      <span className="bottom-0 top-0 w-full  absolute -mt-[17%]">
+        <img
+          src="https://d35aaqx5ub95lt.cloudfront.net/images/splash/lottie/890eb76de9a395b182c1c28322721405.svg"
+          className="h-full"
+        />
+      </span>
+    </div>
+  );
 };
 
-export default wisdom;
+export default Wisdom;
