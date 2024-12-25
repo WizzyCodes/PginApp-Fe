88918737a84060d4e23114ac285cf1be00aace1d
AyodTrader12 <ibrahimpopoola292@gmail.com>
2024-12-25 06:58:09 -0800

    fold

--- src/components/Ibrahim.tsx
+++ src/components/Ibrahim.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Ibrahim = () => {
   return (
-    <div>Ibrahim</div>
+    <div>Ibrahim
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis placeat impedit iusto eaque obcaecati, libero incidunt accusantium nesciunt quisquam itaque. Soluta voluptatum laboriosam facere aliquam illo, ut adipisci autem a!</h1>
+    </div>
   )
 }
 
